588be9dbdb3393a689bc1da072663fba4eac201f
felix1234567890 <blueskyandsea01@gmail.com>
2020-10-03 18:58:21 +0200

    Created React app and recoil setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useRecoilState } from "recoil";
+import { usersState } from "./atom";
 
 function App() {
+  const [{ loading, users }, setUsersData] = useRecoilState(usersState);
+  useEffect(() => {
+    setUsersData((prev) => ({ ...prev, loading: true }));
+    fetch("./users.json").then((data) =>
+      data.json().then((res) => {
+        setTimeout(() => {
+          setUsersData({ users: [...res], loading: false });
+        }, 3000);
+      })
+    );
+  }, [setUsersData]);
+  console.log(loading);
+  console.log(users);
+  if (loading) return <h1>Loading</h1>;
   return (
     <div className="App">
       <header className="App-header">
