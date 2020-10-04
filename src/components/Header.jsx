b1d4fba1e2f045f7fbe429bbe401d21cfe096d07
felix1234567890 <blueskyandsea01@gmail.com>
2020-10-04 17:25:52 +0200

    Introducing translations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 const Header = ({ search }) => {
   const [lang, setLang] = useState("en");
+  const { t, i18n } = useTranslation();
+
   const changeLanguage = () => {
-    if (lang === "en") {
-      setLang("hr");
-    } else {
-      setLang("en");
-    }
+    lang === "en" ? setLang("hr") : setLang("en");
   };
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [i18n, lang]);
   return (
     <header className="header">
-      <div className="header__title">Users search app</div>
+      <div className="header__title">{t("headerTitle")}</div>
       <div className="header__search">
-        <input
-          type="search"
-          placeholder="Search by country"
-          onChange={search}
-        />
+        <input type="search" placeholder={t("searchText")} onChange={search} />
       </div>
       <span onClick={changeLanguage} className="language">
-        EN
+        {t("lng")}
       </span>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { RecoilRoot } from "recoil";
+import i18n from "./i18n";
+import { I18nextProvider } from "react-i18next";
 
 ReactDOM.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>,
+  <I18nextProvider i18n={i18n}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </I18nextProvider>,
   document.getElementById("root")
 );
 
