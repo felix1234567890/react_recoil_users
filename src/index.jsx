268db2b32c599cc32dbd007b337c2fcfc706f0c7
Frane Lukin <frane@Franes-MacBook-Pro.local>
2023-08-25 19:21:33 +0200

    Updated versions and added eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { RecoilRoot } from "recoil";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const domNode = document.getElementById('root') 
+if (!domNode) throw new Error('Failed to find the root element');
+const root = createRoot(domNode)
+root.render(
   <I18nextProvider i18n={i18n}>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </I18nextProvider>,
-  document.getElementById("root")
+  </I18nextProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
